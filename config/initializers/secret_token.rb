7b6da7d49cace81a977e0541fb98f081454ef77e
Justin Geibel <Justin.Geibel@ansaldo-sts.us>
2011-02-26 16:13:23 -0500

    Fresh rails 3.1.0.beta installation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ce2SampleApp::Application.config.secret_token = 'f30ade193ef94f97c64f5e4f9724bfa340d63b585f4678c4b6b838358b8db7ea0c34b4a3cbe1f609cc5f5a58d11d2648417336866337497f1d6f37aa408ec3c5'
